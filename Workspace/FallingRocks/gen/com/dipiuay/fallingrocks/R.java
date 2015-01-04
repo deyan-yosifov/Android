2602abc676cfbf1beeaed7d7f8d48b01b0f7872c
deyan-yosifov <yosifov.deyan@gmail.com>
2015-01-04 13:59:40 +0200

    Renamed some resources and class names

--- Workspace/FallingRocks/gen/com/dipiuay/fallingrocks/R.java
+++ Workspace/FallingRocks/gen/com/dipiuay/fallingrocks/R.java
@@ -1272,7 +1272,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a000d;
-        public static final int button_play_game=0x7f0a0011;
+        public static final int button_monitor_accelerometer=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
         public static final int no_accelerometer_found=0x7f0a000f;
         public static final int title_activity_single_player_game=0x7f0a0013;
