27a66a3d30549136f122b2fda30bd21adaf9fdb4
deyan-yosifov <yosifov.deyan@gmail.com>
2015-01-04 14:11:46 +0200

    Some more resources renamed :)

--- Workspace/FallingRocks/gen/com/dipiuay/fallingrocks/R.java
+++ Workspace/FallingRocks/gen/com/dipiuay/fallingrocks/R.java
@@ -947,6 +947,7 @@ containing a value of this type.
  Minimum width of the search view text entry area. 
          */
         public static final int abc_search_view_text_min_width=0x7f08000c;
+        public static final int accelerometerMonitor_linearLayout_margin=0x7f080017;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Example customization of dimensions originally defined in res/values/dimens.xml
@@ -1080,11 +1081,11 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int acceleromatorValues=0x7f05003d;
-        public static final int accelerometer_length_textView=0x7f050041;
-        public static final int accelerometer_x_coordinate_textView=0x7f05003e;
-        public static final int accelerometer_y_coordinate_textView=0x7f05003f;
-        public static final int accelerometer_z_coordinate_textView=0x7f050040;
+        public static final int acceleromatorValues=0x7f05003c;
+        public static final int accelerometer_length_textView=0x7f050040;
+        public static final int accelerometer_x_coordinate_textView=0x7f05003d;
+        public static final int accelerometer_y_coordinate_textView=0x7f05003e;
+        public static final int accelerometer_z_coordinate_textView=0x7f05003f;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1099,16 +1100,16 @@ containing a value of this type.
         public static final int action_settings=0x7f05005a;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
-        public static final int averageDeltaTime_textView=0x7f050057;
-        public static final int average_gravity_length_textView=0x7f050051;
-        public static final int average_linearAcceleration_length__textView=0x7f050049;
+        public static final int averageDeltaTime_textView=0x7f050056;
+        public static final int average_gravity_length_textView=0x7f050050;
+        public static final int average_linearAcceleration_length__textView=0x7f050048;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int currentDeltaTime_textView=0x7f050055;
-        public static final int currentEventDeltaTime_textView=0x7f050056;
-        public static final int currentEventTime_textView=0x7f050054;
-        public static final int currentTime_textView=0x7f050053;
+        public static final int currentDeltaTime_textView=0x7f050054;
+        public static final int currentEventDeltaTime_textView=0x7f050055;
+        public static final int currentEventTime_textView=0x7f050053;
+        public static final int currentTime_textView=0x7f050052;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1117,30 +1118,30 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
-        public static final int gravityValues=0x7f05004a;
-        public static final int gravity_length_coordinate_textView=0x7f05004e;
-        public static final int gravity_x_coordinate_textView=0x7f05004b;
-        public static final int gravity_y_coordinate_textView=0x7f05004c;
-        public static final int gravity_z_coordinate_textView=0x7f05004d;
+        public static final int gravityValues=0x7f050049;
+        public static final int gravity_length_coordinate_textView=0x7f05004d;
+        public static final int gravity_x_coordinate_textView=0x7f05004a;
+        public static final int gravity_y_coordinate_textView=0x7f05004b;
+        public static final int gravity_z_coordinate_textView=0x7f05004c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int linearAccelerationValues=0x7f050042;
-        public static final int linearAcceleration_length__textView=0x7f050046;
-        public static final int linearAcceleration_x_coordinate_textView=0x7f050043;
-        public static final int linearAcceleration_y_coordinate_textView=0x7f050044;
-        public static final int linearAcceleration_z_coordinate_textView=0x7f050045;
+        public static final int linearAccelerationValues=0x7f050041;
+        public static final int linearAcceleration_length__textView=0x7f050045;
+        public static final int linearAcceleration_x_coordinate_textView=0x7f050042;
+        public static final int linearAcceleration_y_coordinate_textView=0x7f050043;
+        public static final int linearAcceleration_z_coordinate_textView=0x7f050044;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int maxDeltaTime_textView=0x7f050059;
-        public static final int max_gravity_length_textView=0x7f050050;
-        public static final int max_linearAcceleration_length__textView=0x7f050048;
+        public static final int maxDeltaTime_textView=0x7f050058;
+        public static final int max_gravity_length_textView=0x7f05004f;
+        public static final int max_linearAcceleration_length__textView=0x7f050047;
         public static final int middle=0x7f050012;
-        public static final int minDeltaTime_textView=0x7f050058;
-        public static final int min_gravity_length_textView=0x7f05004f;
-        public static final int min_linearAcceleration_length__textView=0x7f050047;
+        public static final int minDeltaTime_textView=0x7f050057;
+        public static final int min_gravity_length_textView=0x7f05004e;
+        public static final int min_linearAcceleration_length__textView=0x7f050046;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
@@ -1164,12 +1165,12 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int timeValues=0x7f050052;
+        public static final int timeValues=0x7f050051;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int welcome_header_id=0x7f05003c;
+        public static final int welcome_header_id=0x7f050059;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1222,8 +1223,8 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int activity_single_player_game=0x7f030019;
+        public static final int activity_accelerometer_monitor=0x7f030018;
+        public static final int activity_main=0x7f030019;
         public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
@@ -1275,7 +1276,7 @@ containing a value of this type.
         public static final int button_monitor_accelerometer=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
         public static final int no_accelerometer_found=0x7f0a000f;
-        public static final int title_activity_single_player_game=0x7f0a0013;
+        public static final int title_activity_accelerometer_monitor=0x7f0a0013;
         public static final int welcome_header=0x7f0a0010;
     }
     public static final class style {
