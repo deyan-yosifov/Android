bb99fab2a65b53b4208e1db508eb0746c5b843c4
deyan-yosifov <yosifov.deyan@gmail.com>
2015-01-05 22:40:52 +0200

    Applying correct canvas transformations. Needs some refactoring!

--- Workspace/BouncingBallGame/src/com/dipiuay/bouncingballgame/Game.java
+++ Workspace/BouncingBallGame/src/com/dipiuay/bouncingballgame/Game.java
@@ -1,38 +1,49 @@
 package com.dipiuay.bouncingballgame;
 
+import android.graphics.RectF;
+
 public class Game {
 	private static final Logger logger = new Logger("Game class");
 	private static final float actualDistanceInMeters = 20;
-	public final float width;
-	public final float height;
+	public final RectF rectangle;
 	public final Ball ball;
 	public final Target target;
 	
 		
 	public Game(float screenRatio){
+		float width, height;
+		
 		if(screenRatio < 1){
-			this.width = actualDistanceInMeters;
-			this.height = this.width / screenRatio;
+			width = actualDistanceInMeters;
+			height = width / screenRatio;
 		}
 		else{
-			this.height = actualDistanceInMeters;
-			this.width = this.height * screenRatio;
+			height = actualDistanceInMeters;
+			width = height * screenRatio;
 		}
-		
+		this.rectangle = new RectF(0, 0, width, height);
 		this.ball = new Ball();
 		this.target = new Target();
 		
-		logger.log("New Game with size: (" + this.width + ", " + this.height + ")");
+		logger.log("New Game with size: (" + this.width() + ", " + this.height() + ")");
 		
 		this.initializePlayground();
 	}
 	
+	public float width() {
+		return this.rectangle.width();	
+	}
+	
+	public float height() {
+		return this.rectangle.height();	
+	}
+	
 	private void initializePlayground(){
-		float minimalSize = Math.min(this.width, this.height);
+		float minimalSize = Math.min(this.width(), this.height());
 		
 		this.ball.radius = minimalSize / 10;
-		this.ball.position.x = this.width * 0.5f;
-		this.ball.position.y = this.height * 0.8f;
+		this.ball.position.x = this.width() * 0.5f;
+		this.ball.position.y = this.height() * 0.8f;
 		
 		this.target.radius = this.ball.radius / 2;
 		this.moveTargetRandomly();
@@ -42,7 +53,7 @@ public class Game {
 		float randomX = (float)Math.random();
 		float randomY = (float)Math.random();
 		
-		this.target.position.x = target.radius / 2 + (this.width - target.radius) * randomX;
-		this.target.position.y = target.radius / 2 + (this.height - target.radius) * randomY;		
+		this.target.position.x = target.radius + (this.width() - 2 * target.radius) * randomX;
+		this.target.position.y = target.radius + (this.height() - 2 * target.radius) * randomY;		
 	}
 }
